84cb8ca17be6fc7732ec4acb7ced17b7930e40e2
moreblessingszimudzi <moreblessingszimudzi@gmail.com>
2026-04-18 23:57:20 +0200

    Responsive Layout + Objects

---
+++ db.js
@@ -0,0 +1,121 @@
+import { createIcons, download } from "lucide";
+
+createIcons({
+  icons: {
+    download,
+  },
+});
+
+<i data-lucide="download"></i>;
+
+const pasta = {
+  icon: "download",
+  title: "Spaghetti Carbonara",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#128100; 4",
+  button: "Pasta",
+};
+console.log(pasta);
+
+const person = {
+  Name: {
+    firstname: "",
+    lastname: "",
+  },
+  age: 65,
+  totem: "Gudo",
+  address: {
+    street: "3rd Street",
+    number: 579,
+    city: "Pretoria",
+    surbub: "Montana",
+  },
+};
+
+console.log(person);
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+
+/*// Remap Method
+
+
+const soup = {
+  icon: "&#>&#128100; 4",
+  title: "Souptt",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#>&#128100; 4",
+  button: "Pasta",
+
+};
+
+const main = {
+  icon: "icon",
+  title: "Main Carbonara",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#>&#128100; 4",
+  button: "main",
+
+};
+
+const breakfast = {
+  icon: "icon",
+  title: "Spaghetti Carbonara",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#>&#128100; 4",
+  button: "Pasta",
+
+};
+
+const salad = {
+  icon: "icon",
+  title: "Spaghetti Carbonara",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#>&#128100; 4",
+  button: "Pasta",
+
+};
+
+const desert = {
+  icon: "icon",
+  title: "Spaghetti Carbonara",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#>&#128100; 4",
+  button: "Pasta",
+
+};
+
+const berryCruble = {
+  icon: "icon",
+  title: "Spaghetti Carbonara",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#>&#128100; 4",
+  button: "Pasta",
+
+};
+
+const shakshuka = {
+  icon: "icon",
+  title: "Spaghetti Carbonara",
+  paragraph:
+    "A rich and creamy Romab classic made with eggs,pecorino and guanciale",
+  icon2: "&#8986;20 min",
+  icon3: "&#>&#128100; 4",
+  button: "Pasta",
+
+};
+ */
